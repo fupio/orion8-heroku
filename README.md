7632f9871fa6bb09ecf5f582790c3870b1fe8030
Mehmet Kose <mehmetkose122@gmail.com>
2019-01-11 21:41:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Orion [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/mehmetkose/orion)
+
+![Orion8](orion8-logo.png)
+
+A blockchain database for BlockStack projects to keep relations on a trustable network which uses Websockets for p2p communication. 
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/fupio/orion8/tree/master)
+
+# Install
+
+## Install Dependencies
+
+yarn
---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoUtil = require('./util/mongo');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+mongoUtil.connectToServer(function(err) {
+	if (err) return console.log(err);
+});
+
+app.get('/', function(req, response) {
+	response.json({"project":"Orion8", "status": "alive"})
+});
+
+const port = process.env.PORT || 5000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
