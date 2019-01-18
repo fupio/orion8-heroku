005f2ba644444ffc397cb522352df6e44cf784dc
Mehmet Kose <mehmetkose122@gmail.com>
2019-01-18 22:50:30 +0300

    get the missing code from gitlab. gitlab saved my ass.

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,5 @@
 import SHA256 from "crypto-js/sha256";
-import Datastore from "nedb";
+import jsonfile from "jsonfile";
 
 class Block {
   constructor(index, timestamp, previousHash, data) {
@@ -9,9 +9,9 @@ class Block {
     this.data = data;
     this.hash = this.calculateHash();
   }
-  calculateHash() {
+  calculateHash = () => {
     return SHA256(
-        this.index +
+      this.index +
         this.timestamp +
         this.previousHash +
         JSON.stringify(this.data)
@@ -21,56 +21,60 @@ class Block {
 
 class Chain {
   constructor() {
-    this.latestBlock = this.createGenesisBlock();
-    this.db = this.setupDatabase();
-    // this.chain = this.db.get("chain").value();
+    this.fileName = "blockchain.db";
+    this.latestBlock = this.setupDatabase();
   }
-  // Setup a database by default array with genesis block.
   setupDatabase = () => {
-    const db = new Datastore({ filename: "blockchain.db", autoload: true });
-    db.find({}, (err, docs) => {
-      if (!err && docs.length == 0) {
-        db.insert([this.latestBlock]);
+    const schema = {
+      chain: [this.createGenesisBlock()],
+      cache: {}
+    };
+    try {
+      const database = jsonfile.readFileSync(this.fileName);
+      return database.chain[database.chain.length - 1];
+    } catch (error) {
+      if (error.errno == -2) {
+        jsonfile.writeFileSync(this.fileName, schema);
       }
-    });
-    return db;
-  };
-  createGenesisBlock = () => new Block(0, Date.now(), "0", { name: "Genesis Block" });
-  getLatestBlock = () => this.latestBlock;
-  getBlockChain = () => {
-    this.db.find({}, (err, docs) => {
-      if (!err) {
-        return err;
-      }
-      console.log("zaaa xd");
-      console.log(docs);
-      return docs;
-    });
-  };
-  addBlock(newBlock) {
-    // console.log(this.latestBlock,   newBlock)
-    // return true
-    if (newBlock && newBlock.index == this.latestBlock.index + 1) {
-      // this.db.get("chain").push(newBlock).write();
-      this.db.insert([newBlock]);
-      this.latestBlock = newBlock;
-      // this.chain.push(newBlock);
-      return true;
+      return schema.chain[0];
     }
-    return false;
+  }
+  createBlock = (index, timestamp, previousHash, data) => {
+    return new Block(index, timestamp, previousHash, data);
+  };
+  createGenesisBlock = () => this.createBlock(0, Date.now(), "0", "Genesis Block");
+  addBlock = (newBlock) => {
+    const database = jsonfile.readFileSync(this.fileName);
+    database.chain.push(newBlock);
+    jsonfile.writeFileSync(this.fileName, database);
+    this.latestBlock = database.chain[database.chain.length -1];
+    return true;
+  };
+  getBlockChain = () => {
+    const database = jsonfile.readFileSync(this.fileName);
+    return database.chain;
   }
   generateNextBlock = blockData => {
-    const previousBlock = this.latestBlock;
-    const index = previousBlock.index + 1;
+    const index = this.latestBlock.index + 1;
     const timestamp = Date.now();
-    const previousHash = previousBlock.hash;
+    const previousHash = this.latestBlock.hash;
     return new Block(index, timestamp, previousHash, blockData);
   };
-  isChainValid = () => {
-    for (let i = 1; i < this.chain.length; i += 1) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
+  isChainValid() {
+    const database = jsonfile.readFileSync(this.fileName);
+    for (let i = 1; i < database.chain.length; i += 1) {
+      const currentBlock = this.createBlock(
+        database.chain[i].index,
+        database.chain[i].timestamp,
+        database.chain[i].previousHash,
+        database.chain[i].data
+      );
+      const previousBlock = this.createBlock(
+        database.chain[i - 1].index,
+        database.chain[i - 1].timestamp,
+        database.chain[i - 1].previousHash,
+        database.chain[i - 1].data
+      );
       if (previousBlock.index + 1 !== currentBlock.index) {
         return false;
       } else if (currentBlock.hash !== currentBlock.calculateHash()) {
@@ -80,7 +84,7 @@ class Chain {
       }
     }
     return true;
-  };
+  }
 }
 
 export default { Block, Chain };
